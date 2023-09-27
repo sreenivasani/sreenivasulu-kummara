375fde1294dc77d6a56c0fc399129bf69985c564
sreenivasani <146092449+sreenivasani@users.noreply.github.com>
2023-09-27 17:42:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var varValue;
+let letvalue;
+
+function varValue(){
+    varValue="10000";
+    document.getElementById("varValue").textContent=varValue;
+    
+}
+
+function letValue(){
+    letValue="20000";
+    document.getElementById("letValue").textContent=letValue;
+    
+}
+function constValue(){
+    constValue="30000";
+    document.getElementById("constValue").textContent=constValue;
+    
+}
+function showAlert(){
+    alert(60000);
+    
+}
+function logAlert(){
+    console.log("Thi is About to create different variables");
+}
